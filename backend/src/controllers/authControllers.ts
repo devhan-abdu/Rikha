e2621b880ba765a4f9d63847e1431079434f3146
devhan-hub <hanabduman@gmail.com>
2025-09-02 11:57:59 +0300

    auth controller

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -105,6 +105,69 @@ const refresh = catchAsync ( async (req: Request, res: Response, next: NextFunct
         });
 })
 
+
+// handleUpdateProfile ,handleprofile ,handleAllUsers ,handleResetPassword ,handleDeleteUser
+// export const handleGetUser = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+//     try {
+
+//         const userId = req.user.userId
+
+//         const userInfo = await getUser(userId)
+//         res.status(200).json({ success: true, user: userInfo });
+
+//     } catch (error) {
+//         next(error);
+//     }
+// }
+
+// export const handleUpdateProfile = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+//     try {
+//         const { name, email }: { name?: string, email?: string } = req.body;
+
+//         if (!name && !email) {
+//             res.status(400).json({ success: false, message: 'At least one field (name or email) is required' });
+//             return;
+//         }
+//         // if(!req?.user){   not needed middleware already handled it  💪
+//         //     res.status(401).json({ success: false, message: 'user not found' });
+//         //     return;
+//         // }
+
+//         const updateduUser = await updateUserProfile(req.user.userId, name, email);
+//         res.status(200).json({ success: true, message: 'profile updated successfully', user: updateduUser });
+
+//     } catch (error) {
+//         next(error);
+//     }
+// }
+
+// export const handleAllUsers = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {    // may add later pagination and search functionality
+//     try {
+//         const users = await getAllUsers();
+//         res.status(200).json({ success: true, users });
+//     } catch (error) {
+//         next(error);
+//     }
+// }
+
+// export const handleDeleteUser = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+
+//     try {
+//         const userId = req.params.id;
+//         if (!userId) {
+//             res.status(400).json({ success: false, message: 'User ID is required' });
+//             return;
+//         }
+//         const deletedUser = await deleteUser(userId);
+
+//         res.status(200).json({ success: true, message: 'User deleted successfully', user: deletedUser });
+//     } catch (error) {
+//         next(error);
+//     }
+
+
+// }
+
 export { register, login, logout, verifyEmail, resetPassword, forgetPassword, refresh}
 
 
