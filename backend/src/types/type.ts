d62ddf0e60a031ec53473a5d4c17aa7758122283
devhan-hub <hanabduman@gmail.com>
2025-10-09 11:02:20 +0300

    feat(order): add Zod schemas for order body

--- backend/src/types/type.ts
+++ backend/src/types/type.ts
@@ -1,3 +1,4 @@
+import z from "zod";
 export type SanityProduct = {
   _id: string;
   title: string;
@@ -14,8 +15,42 @@ export type sanityCategory = {
 }
 
 export type review = {
- userId:number,
- productId:string,
- rating:number,
- comment:string
+  userId: number,
+  productId: string,
+  rating: number,
+  comment: string
 }
+export type ShippingSchema = {
+  country: string,
+  firstName: string,
+  lastName: string,
+  subcity: string,
+}
+
+export const OrderItemSchema = z.object({
+  productId: z.number(),
+  quantity: z.number(),
+});
+
+export const OrderBodySchema = z.object({
+  items: z.array(OrderItemSchema),
+  shippingAddressData: z.object({
+    country: z.string(),
+    firstName: z.string(),
+    lastName: z.string(),
+    subcity: z.string(),
+    woreda: z.string(),
+    houseNumber: z.string(),
+    phoneNumber: z.string().regex(/^(09|07)\d{8}$/, "Invalid Ethiopian phone format"),
+    isDefault: z.boolean().optional(),
+  }),
+  paymentMethod: z.enum(["CASH", "TELEBIRR", "MPSA", "CBEBIRR"]),
+  
+})
+
+
+
+export type OrderItem = z.infer<typeof OrderItemSchema>
+export type OrderBody = z.infer<typeof OrderBodySchema>
+export type ExtendedOrderBody = OrderBody & { userId: number };
+
