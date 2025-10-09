3a2abcde04a825b365c62d6be68003b37860a60a
devhan-hub <hanabduman@gmail.com>
2025-10-09 11:27:08 +0300

    fit: add userReducer

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './slices/cartSlice'
+import userReducer from './slices/authSlice'
 export const store = configureStore({
     reducer:{
        cart:cartReducer,
+       user:userReducer,
     }
 })
 
