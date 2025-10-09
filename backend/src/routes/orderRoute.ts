5b85965d90dea9fb91419fe224ea5620eb7487e9
devhan-hub <hanabduman@gmail.com>
2025-10-09 10:51:22 +0300

    fit: add order route

---
+++ backend/src/routes/orderRoute.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { orderController, verifyTransaction , orderStatusController,getUserOrdersController } from "../controllers/orderController";
+import { isAuth } from "../middleware/isAuth";
+
+const router = express.Router();
+
+router.post('/order' ,isAuth,orderController);
+router.get('/verify-payment/:id' ,verifyTransaction)
+router.get('/order/status',isAuth,orderStatusController )
+router.get('/orders',isAuth,getUserOrdersController  )
+
+
+export {router as orderRouter}
