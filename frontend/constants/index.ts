6108069ec0ce367186561d43ba29e037b1fe957c
devhan-hub <hanabduman@gmail.com>
2025-10-09 11:17:37 +0300

    feat(nav): add navigation data

---
+++ frontend/constants/index.ts
@@ -0,0 +1,25 @@
+ import { Category , Product , NavItem } from "@/interface"
+
+
+ export const navData:NavItem[] = [
+    {
+        id:1,
+        name:'Home',
+        path:'/',
+    },
+    {
+        id:2,
+        name:'Catagory',
+        path:'/category',
+    },
+    {
+        id:3,
+        name:'About',
+        path:'/about',
+    },
+    {
+        id:4,
+        name:'Contact',
+        path:'/contact',
+    },
+]
