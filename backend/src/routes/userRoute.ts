7a60016a0de991c34b617ecd6fa1ff940e228094
devhan-hub <hanabduman@gmail.com>
2025-10-21 23:18:24 +0300

    feat(routes): add user profile routes

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,22 +1,17 @@
 import express from 'express';
 import * as authValidation from '../middleware/authValidation'
-import * as  authController from '../controllers/authControllers'
+import * as  userController from '../controllers/userController'
+import { isAuth } from '../middleware/isAuth';
 
 
 const router = express.Router();
 
-router.post('/auth/register', authValidation.register, authController.register);
-router.post('/auth/verify-email', authValidation.verifyEmail, authController.verifyEmail);
-router.post('/auth/login', authValidation.login, authController.login)
-router.post('/auth/logout', authValidation.logout, authController.logout)
+router.use(isAuth)
 
-router.post('/auth/forgot-password', authValidation.forgetPassword, authController.forgetPassword);
-router.put('/auth/reset-password/:token', authValidation.resetPassword, authController.resetPassword);
-
-router.get('/auth/google', authController.googleAuthRedirect)
-router.get('/auth/google/callback', authController.handleGoogleCallback)
-
-router.post('/refresh-token', authController.refresh);
+router.route("/user/me")
+.get(userController.getUser)
+.put(userController.updateUser)
+.delete(userController.deleteUser);
 
 
 
