b1e23b935c3ca5ba71fd586eb6533cc53bc68fe1
devhan-hub <hanabduman@gmail.com>
2025-10-21 10:34:04 +0300

    refactor(validation): create reusable validate

--- backend/src/middleware/authValidation.ts
+++ backend/src/middleware/authValidation.ts
@@ -1,62 +1,45 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
 import { body, cookie, param, validationResult } from 'express-validator';
 
+export const validate = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+  next();
+};
+
+
 const register: RequestHandler[] = [
    body('name').notEmpty().withMessage('Username is required'),
    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
    body('email').isEmail().withMessage('Please enter a valid email').notEmpty().withMessage('email is required'),
-   ((req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-      next();
-   }) as RequestHandler
+   validate
 ]
 
 const verifyEmail: RequestHandler[] = [
    body('email').isEmail().withMessage('Please enter a valid email').notEmpty().withMessage('email is required'),
    body('otp').notEmpty().withMessage('otp required'),
-   ((req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-      next();
-   }) as RequestHandler
+   validate
 ]
 const login: RequestHandler[] = [
    body('email').isEmail().withMessage('Please enter a valid email').notEmpty().withMessage('email is required'),
    body('password').notEmpty().withMessage('password is  required'),
-   ((req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-      next();
-   }) as RequestHandler
+   validate
 ]
 
 
 const forgetPassword: RequestHandler[] = [
    body('email').isEmail().withMessage('Please enter a valid email').notEmpty().withMessage('email is required'),
-   ((req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-      next();
-   }) as RequestHandler
+   validate
 ]
 
 const resetPassword: RequestHandler[] = [
    param('token').notEmpty().withMessage('token not found'),
    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-   ((req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-      next();
-   }) as RequestHandler
+   validate
 ]
 const logout: RequestHandler[] = [
    cookie('resfresh').notEmpty().withMessage('token not found'),
-   ((req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-      next();
-   }) as RequestHandler
+   validate
 ]
 
 
