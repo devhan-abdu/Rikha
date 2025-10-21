c3d3adce62b274d38c73b6e60df40ba3ed147c09
devhan-hub <hanabduman@gmail.com>
2025-10-21 23:19:25 +0300

    feat(auth): switch authentication from header to cookie

--- backend/src/middleware/isAuth.ts
+++ backend/src/middleware/isAuth.ts
@@ -1,25 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from 'jsonwebtoken';
-import  dotenv from "dotenv"
+import dotenv from "dotenv"
 
 
 dotenv.config();
 
 
 export const isAuth = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization as string
-  if (!authHeader?.startsWith('Bearer ')) {
+  const accessToken = req.cookies?.access;
+  if (!accessToken) {
     return res.status(401).json({ message: "Unauthorized" })
   }
 
-  const token = authHeader.split(' ')[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as JwtPayload;
+    const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET!) as JwtPayload;
     req.user = { userId: decoded.userId, role: decoded.role };
     next();
   } catch (err) {
-    return res.status(403).json({ message: 'Forbidden' });
+    return res.status(403).json({ message: "Forbidden or token expired" });
   }
 }
 
