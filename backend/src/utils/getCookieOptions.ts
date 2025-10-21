a0b5c0b2fdf9b913bc74fc795feb1a017c217846
devhan-hub <hanabduman@gmail.com>
2025-10-21 23:16:25 +0300

    refactor(auth): unify cookie options for access and refresh tokens

--- backend/src/utils/getCookieOptions.ts
+++ backend/src/utils/getCookieOptions.ts
@@ -1,13 +1,16 @@
 import { CookieOptions } from "express";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 dotenv.config();
-const isProduction = process.env.NODE_ENV === 'production';
 
-export const getCookieOptions = (): CookieOptions => ({
+const isProduction = process.env.NODE_ENV === "production";
+
+export const getCookieOptions = (isAccessToken: boolean): CookieOptions => ({
   httpOnly: true,
-  secure: true,
-  sameSite: 'none',
-  maxAge: 7 * 24 * 60 * 60 * 1000,
+  secure: isProduction, 
+  sameSite: isProduction ? "none" : "lax",
+  maxAge: isAccessToken
+    ? 15 * 60 * 1000 
+    : 7 * 24 * 60 * 60 * 1000,
   path: "/",
-})
+});
