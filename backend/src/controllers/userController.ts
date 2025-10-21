d3fcaefd95e646dc39a4994e920fb9070aa4a123
devhan-hub <hanabduman@gmail.com>
2025-10-21 23:14:22 +0300

    fit: add userController

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../utils/catchAsync";
+import prisma from "../config/prisma";
+import * as userServices from "../services/userServices";
+
+
+export interface AuthenticatedRequest extends Request {
+    user?: any;
+}
+
+const getUser = catchAsync(async (req: AuthenticatedRequest, res: Response) => { 
+    const userId = req.user?.userId;
+   const user = await userServices.getUserProfile(userId);
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+})
+
+const updateUser = catchAsync(async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user?.userId;
+    const { name, email } = req.body
+    if(!name && !email){ 
+        return res.status(400).json({ success: false, message: "No data provided for update." });
+    }
+    const updatedUser = await userServices.updateUserProfile(userId, name, email);
+
+    res.status(200).json({
+        success: true,
+        user: updatedUser
+    });
+})
+
+const deleteUser = catchAsync(async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user?.userId;
+    await userServices.deleteUser( userId);
+    res.status(200).json({
+        success: true,
+        message: "User deleted successfully"
+    });
+})
+
+export { getUser, updateUser, deleteUser }
