6c2d29934b6790bb7c6f4a9958ab22ac2e5e62c7
devhan-hub <hanabduman@gmail.com>
2025-10-21 23:28:36 +0300

    feat(routes): register user and auth routers in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,44 +1,41 @@
-import express , { Application, Request, Response, NextFunction } from "express";
-import  dotenv from "dotenv"
+import express, { Application } from "express";
+import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import { errorHandler } from "./middleware/errorhandler";
 import { cartRouter } from "./routes/cartRoute";
 import { productRouter } from "./routes/productRoute";
 import { userRouter } from "./routes/userRoute";
+import { authRouter } from "./routes/authRoute";
 import cors from 'cors'
 import { orderRouter } from "./routes/orderRoute";
 import { addressRouter } from "./routes/addressRoute";
 
-
-
 dotenv.config();
-const app:Application =express() 
+const app: Application = express()
 
 app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true
+    origin: 'http://localhost:3000',
+    credentials: true
 }))
 
-// middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 
-// Routes
-app.use('/api',userRouter)
-app.use('/api' ,cartRouter)
-app.use('/api' , productRouter)
+app.use('/api', authRouter)
+app.use('/api', productRouter)
+
+app.use('/api', userRouter)
+app.use('/api', cartRouter)
 app.use('/api', orderRouter)
 app.use('/api', addressRouter)
 
 
 
 
-// Error handling middleware
 app.use(errorHandler)
 
-// start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
