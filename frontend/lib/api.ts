281c11b1a56ede215ad0f9496afabc059e708fd9
devhan-hub <hanabduman@gmail.com>
2025-10-22 23:19:24 +0300

    feat(api): add response interceptor

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,8 +1,60 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const api =axios.create({
-    baseURL:process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000',
-    withCredentials:true,
+const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+    withCredentials: true,
 });
+
+const apiPublic = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+    withCredentials: true,
+});
+
+
+
+let isRefreshing = false;
+let refreshSubscribers: (() => void)[] = [];
+
+function onRefreshed() {
+    refreshSubscribers.forEach(cb => cb());
+    refreshSubscribers = [];
+}
+
+
+
+api.interceptors.response.use(
+    res => res,
+    async (error) => {
+        const original = error.config;
+        if (error.response?.status === 403 && !original._retry) {
+            original._retry = true;
+
+            if (!isRefreshing) {
+                isRefreshing = true;
+                try {
+                    await apiPublic.post('/auth/refresh');
+
+                    isRefreshing = false;
+                    onRefreshed();
+                    return api(original);
+                } catch (err) {
+                    isRefreshing = false;
+                    window.location.href = '/login';
+                    return Promise.reject(err);
+                }
+            }
+
+            return new Promise((resolve) => {
+                refreshSubscribers.push(() => {
+                    resolve(api(original));
+                });
+            });
+        }
+        if(error.response?.status === 401){
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
