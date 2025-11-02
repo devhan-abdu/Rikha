d3da26a2a5d7b0d9243efafe660a6eb49fdf1b8c
devhan-hub <hanabduman@gmail.com>
2025-11-02 23:09:31 +0300

    using AccountDropdown

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,60 +1,53 @@
-// import { TbDeviceMobileCharging } from "react-icons/tb";
-
 'use client'
-import { ShoppingBag , User ,Search ,TabletSmartphone   } from 'lucide-react';
+import { ShoppingBag, User, Search, TabletSmartphone } from 'lucide-react';
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import { navData } from '@/constants/index';
 import MobileNav from "./MobileNav";
-import {selectTotalQnt } from '@/redux/slices/cartSlice';
-import { selectIsAuthenticated , selectUser } from '@/redux/slices/authSlice';
+import { selectTotalQnt } from '@/redux/slices/cartSlice';
+import { selectIsAuthenticated } from '@/redux/slices/authSlice';
 import { useAppSelector } from '@/redux/hooks';
+import { AccountDropdown } from './AccountDropdown';
 
 
 const Header = () => {
   const pathname = usePathname();
-    const totalQnt = useAppSelector(selectTotalQnt)
-    const user = useAppSelector(selectUser) ;
-    const isAuthenicated = useAppSelector(selectIsAuthenticated)
+  const router = useRouter();
+  const totalQnt = useAppSelector(selectTotalQnt)
+  const isAuthenicated = useAppSelector(selectIsAuthenticated)
 
 
   return (
     <div className="flex justify-between items-center py-4 px-4 md:px-12 lg:px-24 w-full  z-[9999] ">
-      <Link  href ="/" className={`font-cinzel text-primary font-bold  flex items-center gap-1  order-2 md:order-1`}>
+      <Link href="/" className={`font-cinzel text-primary font-bold  flex items-center gap-1  order-2 md:order-1`}>
         <TabletSmartphone className='text-6xl' />
         <p className='text-2xl md:text-4xl'>Rikha</p>
-      </Link> 
+      </Link>
       <nav className="hidden items-center justify-between gap-16 md:flex md:order-2 xl:text-lg ">
         <ul className="flex items-center justify-center gap-6 no-underline  flex-1">
           {
-            navData.map((item) => {
-              return (
-                <li key={item.id}>
-                  <Link href={item.path} className={cn('transition-colors text-lg font-poppins', { "text-primary": pathname === item.path })}>
-                    {item.name}
-                  </Link>
-                </li>
-              )
-            })
+            navData.map((item) => (
+
+              <li key={item.id}>
+                <button onClick={() => router.push(item.path)} className={cn(' text-lg transition-colors ', { "text-primary": pathname === item.path })}>
+                  {item.name}
+                </button>
+              </li>
+            )
+            )
           }
         </ul>
       </nav>
-      <MobileNav/>
-     
-      <div className="flex items-center gap-4 order-3">      
-        <button  className="text-gray-600 hidden md:block cursor-pointer z-10 "  ><Link href='/search'><Search /></Link> </button>
-        {/* user */}
-        {isAuthenicated ? (  
-              <Link href='/profile' aria-label='Login'> 
-              <Avatar className='cursor-pointer text-sm'>
-                {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
-                <AvatarFallback className='uppercase'>{user?.name.slice(0,2)}</AvatarFallback>
-              </Avatar>
-              </Link>
+      <MobileNav />
+
+      <div className="flex items-center gap-4 order-3">
+        <button className="text-gray-600 hidden md:block cursor-pointer z-10 "  ><Link href='/search'><Search /></Link> </button>
+
+        {isAuthenicated ? (
+          <AccountDropdown />
         ) : (
-          <button  className="text-gray-600"  ><Link href='/signup'> <User className="" /></Link></button>
+          <Link href='/login' className='text-gray-600'> <User className="" /></Link>
         )}
 
         <div className="relative  flex items-center">
