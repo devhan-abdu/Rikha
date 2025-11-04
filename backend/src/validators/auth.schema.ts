85c3f9eceb1d5e2ee02b97e878847f1ba1f2cd53
devhan-hub <hanabduman@gmail.com>
2025-11-04 22:28:42 +0300

    feat: add zod userUpdateSchema for profile

--- backend/src/validators/auth.schema.ts
+++ backend/src/validators/auth.schema.ts
@@ -25,3 +25,20 @@ export const resetPasswordSchema = z.object({
   password: z.string().min(6),
 });
 
+export const userUpdateSchema = z.object({
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  username: z.string().optional(),
+  avatarUrl: z.string().optional(),
+  email: z.email().optional(),
+  phoneNumber: z
+    .string()
+    .min(10)
+    .regex(/^(09|07)\d{8}$/)
+    .optional(),
+}).refine(data => Object.keys(data).length > 0, {
+  message: "At least one field must be provided"
+});
+
+
+export type userData = z.infer<typeof userUpdateSchema>
