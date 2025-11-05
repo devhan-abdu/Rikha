e0716da2fe07e78f677aae4cc69c88c051235546
devhan-hub <hanabduman@gmail.com>
2025-11-05 22:55:12 +0300

    ui: add reusable dashboard sidenav

---
+++ frontend/components/sidenav.tsx
@@ -0,0 +1,50 @@
+"use client"
+import React from 'react'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { accountNav } from '@/constants';
+import { usePathname, useRouter } from 'next/navigation';
+import { useAppSelector } from '@/redux/hooks';
+import { selectUser } from '@/redux/slices/authSlice';
+
+
+const SideNav = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const user = useAppSelector(selectUser)
+
+  return (
+    <div className='flex h-full flex-col px-3 py-4 md:px-4 md:py-6 gap-6'>
+      <div className=' flex items-center gap-3 '>
+        <Avatar className="cursor-pointer ">
+          <AvatarFallback className="uppercase">
+            {user?.username?.slice(0, 2)}
+          </AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col">
+          <p className="font-semibold text-gray-800">{user?.username.split(" ")[0]}</p>
+          <p className='text-sm text-gray-500'>{user?.email}</p>
+        </div>
+      </div>
+
+      <div className='flex items-center justify-between gap-4 md:flex-col md:items-start'>
+        {accountNav.map((item) => {
+          const Icon = item.icon
+          const isActive = pathname === item.path
+          return (
+            <button
+              key={item.name}
+              onClick={() => router.push(item.path)}
+              className={`flex items-center gap-2 text-left px-3 py-2 rounded-md transition-all duration-200
+                    ${isActive ? " font-medium text-primary" : "hover:bg-gray-50 text-gray-700"}`}
+            >
+              <Icon size={18} />
+              <span>{item.name}</span>
+            </button>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default SideNav
