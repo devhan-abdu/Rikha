175556b99eb45a0ee8ca14e089fbd7989261849a
devhan-hub <hanabduman@gmail.com>
2025-11-07 23:48:39 +0300

    fit: add password-change endpoint

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -7,9 +7,11 @@ const router = express.Router();
 
 
 router.route("/user/me")
-.get(isAuth,userController.getUser)
-.patch(isAuth,userController.updateUser)
-.delete(isAuth,userController.deleteUser);
+    .get(isAuth, userController.getUser)
+    .patch(isAuth, userController.updateUser)
+    .delete(isAuth, userController.deleteUser);
+
+router.patch("/user/change-password", isAuth, userController.changePassword)
 
 
 
