8b60a3efe0b4e32af02a96681417cc41cd699719
devhan-hub <hanabduman@gmail.com>
2025-11-08 10:16:58 +0300

    fix: escape apostrophes in JSX

--- frontend/components/Empty.tsx
+++ frontend/components/Empty.tsx
@@ -6,7 +6,7 @@ const Empty = () => {
   return (
      <div className='h-[70vh] flex flex-col items-center justify-center gap-4 px-6'>
       <Image src="/images/empty.jpg" width={200} height={200} alt='empty page'/>
-     <p className='text-center'>It looks like you haven't added any items yet. Let's fill it up with great finds</p>
+     <p className='text-center'>It looks like you haven&apos;t added any items yet. Let&apos;s fill it up with great finds</p>
       <Link href='/category' className="font-cinzel bg-primary text-white px-2.5 py-1.5 rounded-sm text-sm">Start Shopping Now</Link>
       </div>
   )
