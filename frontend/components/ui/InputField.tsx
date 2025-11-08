fe1781fa0e63935946a9ac5c356a448f9bd2cc6e
devhan-hub <hanabduman@gmail.com>
2025-11-08 09:45:30 +0300

    style: update InputField label and border appearance

--- frontend/components/ui/InputField.tsx
+++ frontend/components/ui/InputField.tsx
@@ -1,22 +1,40 @@
-export const InputField = ({ label, error, register, name, readOnly, disabled, ...props }: any) => (
-  <div className="space-y-1">
-    <label htmlFor={name} className="text-sm font-medium text-gray-800 block">
-      {label}
-    </label>
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
-    <input
-      id={name}
-      {...register(name)}
-      readOnly={readOnly}
-      disabled={disabled}
-      {...props}
-      className={`w-full rounded-md px-3 py-2 text-sm shadow-sm focus:outline-none ${
-        readOnly || disabled
-          ? "bg-gray-100 cursor-not-allowed text-gray-600"
-          : "focus:ring-2 focus:ring-gray-200 focus:border-gray-200"
-      }`}
-    />
 
-    {error && <p className="text-red-500 text-xs">{error}</p>}
-  </div>
-);
+export const InputField = ({ label, error, register, name, type = "text", readOnly, disabled, ...props }: any) => {
+  const [showPassword, setShowPassword] = useState(false)
+  const isPassword = type === "password"
+
+  return (
+    <div className="space-y-2 relative">
+      <label htmlFor={name} className="text-sm font-medium text-black/60 block ">
+        {label}
+      </label>
+      <div className="relative">
+        <input
+          id={name}
+          type={isPassword && showPassword ? "text" : type}
+          {...register(name)}
+          readOnly={readOnly}
+          disabled={disabled}
+          {...props}
+          className={`w-full rounded-md px-4 py-2 text-sm shadow-sm focus:outline-none border border-slate-200 ${readOnly || disabled
+            ? "bg-gray-100 cursor-not-allowed text-gray-600"
+            : "focus:ring-2 focus:ring-gray-200 focus:border-gray-200"
+            }`}
+        />
+        {
+          isPassword && (
+            <button onClick={() => setShowPassword(!showPassword)} className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700">
+              {showPassword ? <Eye size={18} /> : <EyeOff size={18} />}
+            </button>
+          )
+        }
+      </div>
+
+
+      {error && <p className="text-red-500 text-xs">{error}</p>}
+    </div>
+  )
+}
