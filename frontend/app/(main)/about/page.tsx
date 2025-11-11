e16d7d7d2e063db1333f8ec7470d50ade3e99e91
devhan-hub <hanabduman@gmail.com>
2025-11-12 00:01:46 +0300

    style: increase section min-heigh

--- frontend/app/(main)/about/page.tsx
+++ frontend/app/(main)/about/page.tsx
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <div className="text-foreground-500 p-6  max-w-[1260px] mx-auto" >
 
-            <section className="relative min-h-[30vh] flex items-center justify-center">
+            <section className="relative min-h-[60vh] flex items-center justify-center">
                 <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary opacity-20 blur-[100px]"></div></div>
                 <div className="flex flex-col  items-center justify-center gap-8 ">
                     <h1 className="font-cinzel text-center text-2xl sm:text-3xl  lg:text-4xl  font-bold  capitalize ">WELCOME TO RIKHA - YOUR ONE-STOP APPLE STORE</h1>
--- frontend/app/(main)/contact/page.tsx
+++ frontend/app/(main)/contact/page.tsx
@@ -4,7 +4,7 @@ import Contact from "@/components/Contact"
 const ContactPage = () => {
     return (
         <div className="px-4 md:px-6 py-6">
-            <section className="relative min-h-[30vh] flex items-center justify-center">
+            <section className="relative min-h-[60vh] flex items-center justify-center">
                 <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary opacity-20 blur-[100px]"></div></div>
                 <div className="flex flex-col  items-center justify-center gap-8 ">
                     <h1 className="font-cinzel text-center text-2xl sm:text-3xl  lg:text-4xl  font-bold  capitalize ">WE&apos;VE GOT A GREAT FEALING ABOUT THIS</h1>
