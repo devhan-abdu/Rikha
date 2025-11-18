8bfc54e55a8595569fe035c50ae4908c396fee6a
devhan-hub <hanabduman@gmail.com>
2025-11-18 15:41:30 +0300

    feat(category): make full card clickable

--- frontend/components/CategorySection.tsx
+++ frontend/components/CategorySection.tsx
@@ -10,12 +10,16 @@ export const CategorySection = async ({
     const categories = await fetchCategories();
 
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-x-3 gap-y-6 max-w-fit mx-auto">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-5 md:gap-8  max-w-fit mx-auto">
             {categories.length === 0 ? (
                 <p className="text-gray-500"> No Category Found</p>
             ) : (
                 categories.map((category) => (
-                    <div
+                    <Link
+                        href={{
+                            pathname: "/category",
+                            query: { slug: category.slug },
+                        }}
                         className="mx-auto rounded-2xl flex flex-col items-center justify-center "
                         key={category.id}
                     >
@@ -23,23 +27,20 @@ export const CategorySection = async ({
                             <Image
                                 src={category.image}
                                 alt="Picture of the author"
-                                width={100}
-                                height={100}
+                                width={140}
+                                height={140}
                                 className="object-contain w-full h-full"
                             />
                         </div>
-                        <div className="h-full flex flex-col justify-between"></div>
-                        <p className="font-cinzel capitalize mt-3">{category.name}</p>
-                        <Link
-                            href={{
-                                pathname: "/category",
-                                query: { slug: category.slug },
-                            }}
+                        <div className="h-full flex flex-col justify-between items-center gap-1 mt-4">
+                        <p className=" capitalize text-base md:text-lg ">{category.name}</p>
+                        <p
                             className="text-sm text-primary hover:scale-105 cursor-pointer transition-all duration-300"
                         >
                             Explore more
-                        </Link>
-                    </div>
+                        </p>
+                        </div>
+                    </Link>
                 ))
             )}
         </div>
